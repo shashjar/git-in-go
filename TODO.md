6802d95ccd479abe03123e7c8e137a4ea5699c70
Shashank Jarmale <shashankjarmale1@gmail.com>
2025-01-24 19:12:56 -0500

    Minor updates

--- TODO.md
+++ TODO.md
@@ -1,5 +1,9 @@
 # TODO List for Git in Go
 
+## General
+
+- [ ] Reach parity with actual Git - create/maintain all the same files & directories, make commands have the same behavior, etc. Refer to https://blog.meain.io/2023/what-is-in-dot-git/ and other resources in the "Initialize the .git directory" stage.
+
 ## Housekeeping/Tech Debt
 
 - [ ] Update [README](README.md) with information about implementation and usage
--- mygit/main.go
+++ mygit/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 const REPO_DIR = "repo/"
 
+func configureLogger() {
+	log.SetFlags(0)
+}
+
 // Usage: ./run.sh <command> [<args>...]
 func main() {
+	configureLogger()
+
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "Usage: ./run.sh <command> [<args>...]\n")
 		os.Exit(1)
@@ -18,16 +26,20 @@ func main() {
 	case "init":
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 			if err := os.MkdirAll(REPO_DIR+dir, 0755); err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+				log.Fatalf("Error creating directory: %s\n", err)
 			}
 		}
 
 		headFileContents := []byte("ref: refs/heads/main\n")
 		if err := os.WriteFile(REPO_DIR+".git/HEAD", headFileContents, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing HEAD file: %s\n", err)
+			log.Fatalf("Error writing HEAD file: %s\n", err)
 		}
 
-		fmt.Println("Initialized git repository")
+		absPath, err := filepath.Abs(REPO_DIR + ".git")
+		if err != nil {
+			log.Fatalf("Error getting absolute path of Git repository: %s\n", err)
+		}
+		log.Printf("Initialized empty Git repository in %s\n", absPath)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
